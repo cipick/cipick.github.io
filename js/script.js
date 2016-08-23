72a8bb1bc2cb2010eb2c7be514567045fff0a315
Cipick <platica.ciprian@gmail.com>
2016-08-24 00:02:35 +0300

    Add tilt function and fix viewport

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function tilt (x, y) {
+  console.log(x, y);
+  document.getElementById('svg-object').style.transform = 'translateX(' + (-1 * x) + 'px)';
+}
+
+if (window.DeviceOrientationEvent) {
+    window.addEventListener("deviceorientation", function () {
+        tilt([event.beta, event.gamma]);
+    }, true);
+} else if (window.DeviceMotionEvent) {
+    window.addEventListener('devicemotion', function () {
+        tilt([event.acceleration.x * 2, event.acceleration.y * 2]);
+    }, true);
+} else {
+    window.addEventListener("MozOrientation", function () {
+        tilt([orientation.x * 50, orientation.y * 50]);
+    }, true);
+}
