1dc925ee6b22e5d1174e8c4d78aefb76a91328b4
Cipick <platica.ciprian@gmail.com>
2016-08-24 23:21:04 +0300

    add query why not?

--- js/script.js
+++ js/script.js
@@ -1,23 +1,24 @@
-function tilt (args) {
-  document.body.scrollLeft += Math.floor(args[1] * 5 + 400);
-  document.getElementById('log').innerHTML = document.body.scrollWidth;
-  // var tiltElement = document.getElementById('baloon-1');
-  // tiltElement.style.transform = "rotateX(" + (-Math.floor(args[0])) + "deg) rotateY(" + (-Math.floor(args[1])) + "deg)";
-  document.getElementById('svg-object').style.transform = "translateX(" + (Math.floor(args[0] + 300)) + "deg)";
+$(document).ready(function () {
+  function tilt (args) {
+    document.getElementById('log').innerHTML = args[1];
+    
+    $('html, body').animate({
+      scrollLeft: $('html, body').offset().left - ( $(window).width() - $('html, body').outerWidth(true) ) / 2 + args[1]
+    }, 200);
 
-  // document.getElementById('svg-object').style.transform = 'translateX(' + (Math.floor(x)) + 'px)';
-}
+  }
 
-if (window.DeviceOrientationEvent) {
-    window.addEventListener("deviceorientation", function () {
-        tilt([event.beta, event.gamma]);
-    }, true);
-} else if (window.DeviceMotionEvent) {
-    window.addEventListener('devicemotion', function () {
-        tilt([event.acceleration.x * 2, event.acceleration.y * 2]);
-    }, true);
-} else {
-    window.addEventListener("MozOrientation", function () {
-        tilt([orientation.x * 50, orientation.y * 50]);
-    }, true);
-}
+  if (window.DeviceOrientationEvent) {
+      window.addEventListener("deviceorientation", function () {
+          tilt([event.beta, event.gamma]);
+      }, true);
+  } else if (window.DeviceMotionEvent) {
+      window.addEventListener('devicemotion', function () {
+          tilt([event.acceleration.x * 2, event.acceleration.y * 2]);
+      }, true);
+  } else {
+      window.addEventListener("MozOrientation", function () {
+          tilt([orientation.x * 50, orientation.y * 50]);
+      }, true);
+  }
+});
