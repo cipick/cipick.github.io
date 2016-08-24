104e2368d09f5e6a50fec0b6aa206d80156889f6
Cipick <platica.ciprian@gmail.com>
2016-08-24 22:47:18 +0300

    tilt sag

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 function tilt (args) {
-  document.body.scrollLeft += Math.floor(args[0]);
+  // document.body.scrollLeft += Math.floor(args[0]);
   document.getElementById('log').innerHTML = Math.floor(args[0]);
+  var tiltElement = document.getElementById('svg-object');
+  tiltElement.style.transform = "rotateX(" + (-Math.floor(args[0])) + "deg) rotateY(" + (-Math.floor(args[1])) + "deg)";
+
   // document.getElementById('svg-object').style.transform = 'translateX(' + (Math.floor(x)) + 'px)';
 }
 
